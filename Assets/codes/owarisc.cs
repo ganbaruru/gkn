8d7f56db27af76a9c3fd35cbc5e20d10afd7ca34
shuta1123 <shutay1123@yahoo.co.jp>
2024-07-11 20:26:28 +0900

    syokika

--- Assets/codes/owarisc.cs
+++ Assets/codes/owarisc.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -14,7 +15,22 @@ public class owarisc : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Return)){
+        if(Input.GetKey(KeyCode.LeftShift)){
+            bpm100.taiminngu=false;
+            bpm100.haato=0;
+            bpm100.sinmaok=true;
+            dameji.damejiok=false;
+            dameji.ldie=1;
+            dameji.mdie=1;
+            dameji.rdie=1;
+            dameji.taokazu=0;
+            dameji.ukekazu=0;
+            points.point=0;
+            points.cp=true;
+            sinechange2.killkazu=0;
+            spown.lspo=1;
+            spown.mspo=1;
+            spown.rspo=1;
             SceneManager.LoadScene("Title");
         }
     }
